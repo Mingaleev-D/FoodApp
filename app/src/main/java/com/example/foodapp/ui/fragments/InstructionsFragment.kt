c73f72ef6418604381f7633f03965b6083857927
mingaleev.d92@mail.ru <tatarstan1204BTc)>
2022-09-30 16:13:42 +0300

    create InstructionsFragment.kt

--- app/src/main/java/com/example/foodapp/ui/fragments/InstructionsFragment.kt
+++ app/src/main/java/com/example/foodapp/ui/fragments/InstructionsFragment.kt
@@ -5,56 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import com.example.foodapp.R
+import com.example.foodapp.databinding.FragmentIngredientsBinding
+import com.example.foodapp.databinding.FragmentInstructionsBinding
+import com.example.foodapp.models.Result
+import com.example.foodapp.utils.Constants
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [InstructionsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class InstructionsFragment : Fragment() {
-  // TODO: Rename and change types of parameters
-  private var param1: String? = null
-  private var param2: String? = null
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    arguments?.let {
-      param1 = it.getString(ARG_PARAM1)
-      param2 = it.getString(ARG_PARAM2)
-    }
-  }
+  private var mBinding: FragmentInstructionsBinding? = null
+  private val binding get() = mBinding!!
 
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
-  ): View? {
-    // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_instructions, container, false)
+  ): View {
+    mBinding = FragmentInstructionsBinding.inflate(layoutInflater)
+    return binding.root
   }
 
-  companion object {
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment InstructionsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    @JvmStatic
-    fun newInstance(param1: String, param2: String) =
-      InstructionsFragment().apply {
-        arguments = Bundle().apply {
-          putString(ARG_PARAM1, param1)
-          putString(ARG_PARAM2, param2)
-        }
-      }
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    val args = arguments
+    val myBundle: Result? = args?.getParcelable(Constants.RECIPE_RESULT_KEY)
+
+    binding.instructionsWebView.webViewClient = object : WebViewClient() {}
+    val websiteUrl: String = myBundle!!.sourceUrl
+    binding.instructionsWebView.loadUrl(websiteUrl)
   }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    mBinding = null
+  }
+
+
 }
